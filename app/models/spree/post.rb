6483885839e1a51962a421499c72da3c61c3a1a4
illnino ho <illnino.giggs@gmail.com>
2013-04-30 23:50:29 +0800

    mass-assign fixed

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,5 +1,7 @@
 class Spree::Post < ActiveRecord::Base
-  validates_presence_of :description
+	attr_accessible :title, :description, :published
+
+  	validates_presence_of :description
 #  validates_length_of :description, :maximum => 150
 
   scope :published, lambda { where(:published => true) }
